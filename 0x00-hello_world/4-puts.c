d7b27403ddeba3f63e9a9df63c04863a72c0ee73
root <you@example.com>
2022-09-09 01:18:50 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <studio.h>
+/**
+*main
+*
+*Description: using the main function 
+*this program prints "Programming is like building a multilingual puzzle"
+*Return:0
+*/
+int main(void)
+{
+    puts("\"Programming is like like a building a multilingual puzzle ");
+return (0);
+}
