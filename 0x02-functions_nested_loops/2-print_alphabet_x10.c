71a068ec3caa71372ddf4f242dc17c26e68c7011
root <you@example.com>
2022-09-15 17:49:09 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_alphabet_×10 - function to print abc 10 times
+*
+* Return: 0
+*/
+char a;
+char b;
+for (b = 0; b<= 9; b++)
+{
+for (a = 'a'; a  <= 'z'; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
+}
