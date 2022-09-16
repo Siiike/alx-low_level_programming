641e362dbc5751b7d76e5eba30ffd6a5d4cc31fc
Siiike <111066416+Siiike@users.noreply.github.com>
2022-09-16 03:44:37 +0100

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
 * _abs - function that computes the absolute value of an integer
 * @c: Is the int that will use for the argument of the function
@@ -11,5 +12,7 @@ if (c > 0 || c == 0)
 return (c);
 }
 else
+{
 return (c * -1);
+}
 }
