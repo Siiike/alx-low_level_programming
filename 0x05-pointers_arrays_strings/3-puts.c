741b781ba4c330d6b759b3c894266778456a3b29
root <you@example.com>
2022-09-20 11:13:46 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _puts - prints a string
+* @str - a string
+* Return: the length of the string
+*/
+void _puts(char *str)
+int = a;
+
+while(str[a] !=  '\0');
+{
+_putchar(str[a]);
+a++;
+}
+_putchar('\n');
+}
