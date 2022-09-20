0deb77986462d44e57fdec0a4b765153c149a249
root <you@example.com>
2022-09-20 11:21:53 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
-* rest_to_98 - takes the pointer and updates value
-* @n: points to n to be changed
-* Return: 0 is success
+*  reset_to_98 - takes the pointer and updates the value
+*  @n: pointer to n to be updated
+*  Return: 0 is success
 */
+
 void reset_to_98(int *n)
 {
-*n = 98;
+	*n = 98;
 }
