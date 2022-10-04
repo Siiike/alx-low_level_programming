ba7a14b36b09ac3be2fec11fa1ec4f34a33697b2
Siiike <111066416+Siiike@users.noreply.github.com>
2022-10-04 01:53:06 +0100

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -4,7 +4,7 @@
 * @s: the string to be printed
 * Return: the length of the string
 */
-int _strlen_recirsion(char *s)
+int _strlen_recursion(char *s)
 {
 if (s[0] != '\0')
 return (1 + _strlen_recursion(s + 1));
