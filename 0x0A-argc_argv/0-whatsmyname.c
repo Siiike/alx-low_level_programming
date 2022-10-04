110c21d05f93fea5efb9ddca9f18a5ea776024d5
root <you@example.com>
2022-10-04 16:30:53 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define UNUSED(x) (void)(x)
+/**
+* main - main function
+* @argc: argumentc
+* @argv: vector of arguments
+* Return: always 0
+*/
+int main(int argc, char *argc[])
+{
+UNUSED(argc)
+printf("s\n", argc[0]);
+return (0);
+}
