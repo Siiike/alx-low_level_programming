bf19664c6cc4450973a1b179a41f379937abad21
Siiike <111066416+Siiike@users.noreply.github.com>
2022-10-06 04:40:16 +0100

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,7 +14,11 @@ int index, len = 0;
 
 if (str == NULL)
 return (NULL);
-
+for (index = 0; str[index]; index++)
+len++;
+duplicate= malloc(sizeof(char) * (len  + 1));
+If (duplicate== NULL)
+return (NULL);
 for (index = 0; str[index]; index++)
 duplicate[index] = str[index];
 
